964b97fcc297ea6dd4da977dd14b539ebe3f077e
sergeybell <sergey.bell13@gmail.com>
2021-06-14 19:46:09 +0300

    Add OptiM installer w examples and html

---
+++ optimx-generic-linux-x86_64-20190225/help/htmldoc/header.js
@@ -0,0 +1,15 @@
+document.write('\
+<link rel="stylesheet" type="text/css" href="css/optim.css" media="screen" />\
+<script type="text/javascript" \
+  src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML">\
+</script> \
+<script type="text/x-mathjax-config"> \
+MathJax.Hub.Config({\
+  tex2jax: {\
+    inlineMath: [["$","$"], ["\\\\(","\\\\)"]],\
+    displayMath: [["$$","$$"], ["\\\\[","\\\\]"]],\
+    processEscapes: true \
+  }\
+});\
+</script>\
+');
---
+++ optimx-generic-linux-x86_64-20190225/help/htmldoc/scripts/mathjax.js
@@ -0,0 +1,14 @@
+<!-- Uncomment this to use a local MathJax -->
+<!-- <script type="text/javascript" src="./MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script> -->
+
+<!-- Get MathJax from an external server --> 
+
+<script type="text/javascript"
+   src="http://cdn.mathjax.org/mathjax/latest/MathJax.js">
+</script>
+
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+});
+</script>  
